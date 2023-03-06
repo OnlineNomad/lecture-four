0fb3f59fe02a2df7eda6c1db39b9576b6608dba5
OnlineNomad <singularity123321@gmail.com>
2023-03-06 15:11:20 +0400

    Add task three of the assignment

--- js/script.js
+++ js/script.js
@@ -5,5 +5,20 @@ function fn(a=1, b=1, ...args) {
     });
     const numbers =[a+b, total];
     return numbers;
+};
+
+const user = {
+    fullName: 'User',
+    username: 'KALE'
+};
+const user2 = {
+    fullName: 'user2'
 }
+function newObjCreator(...obj) {
+    const newObj = {
+        ...obj
+    };
+    return newObj;
+}
+console.log(newObjCreator(user, user2));
 console.log(fn(1,2,3,4,5, 6));
